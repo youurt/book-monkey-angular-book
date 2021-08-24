8ce1674f6a63478962ca9ad3bc51213d434b5677
youurt <ugurtigu@gmail.com>
2021-08-24 22:09:51 +0300

    adds http client

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '@app/shared/book';
 import { BookStoreService } from '@app/shared/book-store.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'bm-book-details',
@@ -11,14 +11,28 @@ import { ActivatedRoute } from '@angular/router';
 export class BookDetailsComponent implements OnInit {
   book: Book;
 
-  constructor(private bs: BookStoreService, private route: ActivatedRoute) {}
+  constructor(
+    private bs: BookStoreService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const params = this.route.snapshot.paramMap;
+    this.bs.getSingle(params.get('isbn')).subscribe((b) => (this.book = b));
+  }
 
   getRating(num: number): Array<number> {
     return new Array(num);
   }
 
-  ngOnInit(): void {
-    const params = this.route.snapshot.paramMap;
-    this.bs.getSingle(params.get('isbn')).subscribe((b) => (this.book = b));
+  removeBook(): void {
+    if (confirm('Buch wirklich löschen?')) {
+      this.bs
+        .remove(this.book.isbn)
+        .subscribe(() =>
+          this.router.navigate(['../'], { relativeTo: this.route })
+        );
+    }
   }
 }
